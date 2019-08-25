11ac32b06e0c621fd15d365f09a11f4e688a42cb
Brent <brent.tudas@gmail.com>
2019-08-25 13:50:22 +0800

    Added Base ERC777 Interfaces and Hooks

---
+++ contracts/Common.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.5.3;
+
+contract CommonConstants {
+    bytes32 constant private TOKEN_SENDER_HASH = 0x29ddb589b1fb5fc7cf394961c1adf5f8c6454761adf795e67fe149f658abe895;
+    bytes32 constant private TOKEN_RECIPIENT_HASH = 0xb281fc8c12954d22544db45de3159a39272895b169a852b314f9cc762e44c53b;
+}
